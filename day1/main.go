d7b99124bfa86a7a907cde6505b6ca7be32a0e68
Andrew <38687378+andrew1044@users.noreply.github.com>
2023-12-09 22:56:22 +1100

    day 1 part 2

--- day1/main.go
+++ day1/main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"strings"
 	"unicode"
 )
 
@@ -27,8 +28,77 @@ func lastDigit(line string) int {
 	return 0
 }
 
-func main() {
-	ReadFile, err := os.Open("input_data")
+func convertTerms(line string) (out string) {
+	conversionMap := map[string]int{
+		"one":   1,
+		"two":   2,
+		"three": 3,
+		"four":  4,
+		"five":  5,
+		"six":   6,
+		"seven": 7,
+		"eight": 8,
+		"nine":  9,
+	}
+
+	finalIntegers := []int{}
+	for charIndex, char := range line {
+		if digit, err := strconv.Atoi(string(char)); err == nil {
+			finalIntegers = append(finalIntegers, digit)
+		} else {
+			for strNumber, number := range conversionMap {
+				// line[charIndex:] is a substring starting at charIndex onwards
+				// strNumber represents the number as a string in the map
+				// so this is "if the following characters from the index match the key in the map"
+				if strings.HasPrefix(line[charIndex:], strNumber) {
+					finalIntegers = append(finalIntegers, number)
+					break
+				}
+			}
+		}
+	}
+
+	// above produces a '[]int{}', convent to a string so the firstDigit and lastDigit functions can be used
+	strDigits := make([]string, len(finalIntegers))
+	for i, digit := range finalIntegers {
+		strDigits[i] = strconv.Itoa(digit)
+	}
+
+	return strings.Join(strDigits, "")
+}
+
+func partTwo(filename string) (total int) {
+
+	ReadFile, err := os.Open(filename)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer ReadFile.Close()
+
+	fileScanner := bufio.NewScanner(ReadFile)
+
+	sum := 0
+
+	for fileScanner.Scan() {
+		// line := fileScanner.Text()
+		convertedLine := convertTerms(fileScanner.Text())
+		// fmt.Print(line + " ---> " + convertedLine + "\n")
+
+		firstDig := firstDigit(convertedLine)
+		lastDig := lastDigit(convertedLine)
+
+		result := firstDig*10 + lastDig
+		// fmt.Println("result: ", result)
+		sum += result
+	}
+
+	// fmt.Println("Sum: ", sum)
+	return sum
+}
+
+func partOne(filename string) (total int) {
+	ReadFile, err := os.Open(filename)
 
 	if err != nil {
 		log.Fatal(err)
@@ -47,7 +117,7 @@ func main() {
 		result := firstDig*10 + lastDig
 		sum += result
 
-		fmt.Println(strconv.Itoa(result))
+		// fmt.Println(strconv.Itoa(result))
 		index++
 	}
 
@@ -56,6 +126,13 @@ func main() {
 	}
 
 	// first half of question
-	fmt.Println("Sum: ", sum)
+	// fmt.Println("Sum: ", sum)
+	return sum
+}
 
+func main() {
+	p1 := partOne("data/input_data")
+	fmt.Println("Part One Result: ", p1)
+	p2 := partTwo("data/input_data")
+	fmt.Println("Part Two Result: ", p2)
 }
